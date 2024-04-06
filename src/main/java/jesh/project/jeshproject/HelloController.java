926e31d53d83e4e45f8131ba28ca37087746dbb0
elizaaajane <n11303816@qut.edu.au>
2024-04-06 16:00:55 +1000

    changed initial application, created Main-View and MainController

--- src/main/java/jesh/project/jeshproject/HelloController.java
+++ src/main/java/jesh/project/jeshproject/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to SleepWell, click here to tell us about your needs");
     }
 }
