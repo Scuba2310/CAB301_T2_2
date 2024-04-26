7546d813d61805d24a46d3cba033912b67ff3a14
Harry Dyba <minecraftminer8888@outlook.com>
2024-04-26 17:54:29 +1000

    added main page

---
+++ src/main/java/jesh/project/jeshproject/MainPage.java
@@ -0,0 +1,33 @@
+package jesh.project.jeshproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+public class MainPage {
+    @FXML private Button nightModeButton;
+    @FXML private Slider brightnessSlider;
+    @FXML private Text brightnessLevelTitle;
+    @FXML private Text endTimeText;
+    @FXML private Text startTimeText;
+    @FXML private Text timelineName;
+    @FXML private Button saveButton;
+    @FXML private VBox infoBox;
+    @FXML private Button signOutButton;
+    @FXML private Button profileButton;
+    @FXML private Button settingsButton;
+    @FXML private Slider timelineSlider;
+
+
+
+
+}
