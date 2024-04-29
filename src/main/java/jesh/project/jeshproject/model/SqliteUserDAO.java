fa067862bcd36c7536edceeb852aacf797409e22
elizaaajane <132536408+elizaaajane@users.noreply.github.com>
2024-04-29 15:46:06 +1000

    creating sql database connection

---
+++ src/main/java/jesh/project/jeshproject/model/SqliteUserDAO.java
@@ -0,0 +1,4 @@
+package jesh.project.jeshproject.model;
+
+public class SqliteContactDAO implements IUserDAO {
+}
--- src/main/java/jesh/project/jeshproject/model/mockDB.java
+++ src/main/java/jesh/project/jeshproject/model/mockDB.java
@@ -15,13 +15,25 @@ public class mockDB {
         addUser( new User("John", "Doe", "01/01/1992", "johndoe@example.com", "john_doe92", "password") );
     }
 
-    public void getUsers() {
-
-    }
     public static void addUser(User user) {
         user.setId(autoIncrementId);
         autoIncrementId++;
         users.add(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users);
     }
 
     // public void updateUser(User user) { }
